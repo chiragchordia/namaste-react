c89fc20a882921ddd6f8e39f86a587f00d19bd3d
chirag <cjain9497@gmail.com>
2025-09-07 21:33:14 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { className: "heading" },
+  "hello world from react"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
